256ea1433a754571c2d7a51c83582be8327b22a3
unknown <flowershirtsman@gmail.com>
2019-10-03 21:28:25 -0400

    edited-schema

--- data/schema.sql
+++ data/schema.sql
@@ -4,7 +4,7 @@ USE alleydb;
 
 CREATE TABLE userdata
 (
-    id int NOT NULL
+    user_id int NOT NULL
     AUTO_INCREMENT,
     username VARCHAR
     (20) NOT NULL,
@@ -15,26 +15,26 @@ CREATE TABLE userdata
     (id)
 );
 
-    CREATE TABLE friendlist
+    CREATE TABLE friendslist
     (
-        friend_id int NOT NULL
-        AUTO_INCREMENT,
-    friendname VARCHAR
-        (20),
-    announcements VARCHAR
-        (200),
-    PRIMARY KEY
-        (id)
-);
+        user_id INT,
+        friend_id INT NOT NULL,
+        foreign key (user_id) REFERENCES userdata(user_id),
+        ignore BOOLEAN DEFAULT FALSE
+    )
 
-        CREATE TABLE messages
+    CREATE TABLE messages
+    (
+        message_id INT NOT NULL
+        AUTO_INCREMENT PRIMARY KEY, message VARCHAR
+        (200), readstatus BOOLEAN DEFAULT FALSE, user_id int, foreign key
+        (user_id) REFERENCES userdata
+        (user_id), author_id INT NOT NULL);
+
+        create table userlistings
         (
-            message_id int NOT NULL
-            AUTO_INCREMENT,
-    message VARCHAR
-            (200) NOT NULL,
-    saved BOOLEAN NOT NULL, 
-    flagged BOOLEAN NOT NULL, 
-    PRIMARY KEY
-            (message_id)
-);
+            item_id INT NOT NULL
+            auto_increment PRIMARY KEY, item_name VARCHAR
+            (30), price INT NOT NULL, user_id INT, foreign key
+            (user_id) REFERENCES userdata
+            (user_id), sold BOOLEAN DEFAULT FALSE, buyer_id INT);
